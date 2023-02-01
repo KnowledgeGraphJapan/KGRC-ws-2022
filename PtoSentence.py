de7b84d0e685e9f160d9112008f6f7c2baebd712
Takanori Ugai <ugai@jp.fujitsu.com>
2023-02-01 13:46:22 +0900

    Update PtoSentence.py

--- PtoSentence.py
+++ PtoSentence.py
@@ -1,7 +1,8 @@
 import re
 import glob
+import json
 
-def printSentence(filepath):
+def printSentence(filepath, df):
     with open(filepath, "r", encoding="utf-8") as f:
         lines = f.readlines()
         print("Mr. Avator ", end="")
@@ -13,32 +14,39 @@ def printSentence(filepath):
             if(m is not None):
                 gr = m.groups()
 #                print("Char1 ", end="")
-                print(gr[0].lower(), end=" ")
+                verb = gr[0].lower()
+                if(verb in df):
+                    verb = df[verb]
+                print(verb, end=" ")
                 if (gr[2] is None):
                     print("and ", end="")
                 if (gr[2] is not None):
-                    print(gr[2], end=" ")
+                    print("the " + gr[2], end=" ")
                     if (gr[4] is None):
                         print("and ", end="")
                     if (gr[4] is not None):
-                        print(gr[4], end=" and ")
+                        print("the " + gr[4], end=" and ")
             m = re.match(
                 r'^<.+>[ \t]+\[([a-zA-Z]+)\][ \t\n]+(<([a-zA-Z]+)>[ \t]+\([0-9]+\))?[ \t\n]*(<([a-zA-Z]+)>[ \t]+\([0-9]+\))?',
                 lines[i].lower())
             if(m is not None):
                 gr = m.groups()
 #                print("Char1 ", end="")
-                print(gr[0], end=" ")
+                verb = gr[0].lower()
+                if(verb in df):
+                    verb = df[verb]
+                print(verb, end=" ")
                 if (gr[2] is None):
                     print("and ", end="")
                 if (gr[2] is not None):
-                    print(gr[2], end=" ")
+                    print("the " + gr[2], end=" ")
                     if (gr[4] is None):
                         print("and ", end="")
                     if (gr[4] is not None):
-                        print(gr[4], end=" and ")
+                        print("the " + gr[4], end=" and ")
         print("done.")
 
-files = glob.glob("./KGRC-RDF/Program/**/*.txt")
+with open('replace.json') as f:
+    df = json.load(f)files = glob.glob("./KGRC-RDF/Program/**/*.txt")
 for filepath in files:
     printSentence(filepath)
